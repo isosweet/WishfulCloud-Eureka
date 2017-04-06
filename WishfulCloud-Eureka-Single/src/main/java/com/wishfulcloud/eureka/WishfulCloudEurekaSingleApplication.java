182578bcfe1c44f9fafb7678a53f5b0b9f165a37
isosweet <405438526@qq.com>
2017-04-06 16:13:03 +0800

    Eureka

---
+++ WishfulCloud-Eureka-Single/src/main/java/com/wishfulcloud/eureka/WishfulCloudEurekaSingleApplication.java
@@ -0,0 +1,12 @@
+package com.wishfulcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishfulCloudEurekaSingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishfulCloudEurekaSingleApplication.class, args);
+	}
+}
