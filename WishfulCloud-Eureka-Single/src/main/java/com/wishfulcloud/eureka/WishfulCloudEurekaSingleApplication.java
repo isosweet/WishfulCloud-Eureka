fc43fdb8d6a6cb099272aa7e91947fa3193b40cf
isosweet <405438526@qq.com>
2017-04-06 17:55:12 +0800

    Single Eureka

--- WishfulCloud-Eureka-Single/src/main/java/com/wishfulcloud/eureka/WishfulCloudEurekaSingleApplication.java
+++ WishfulCloud-Eureka-Single/src/main/java/com/wishfulcloud/eureka/WishfulCloudEurekaSingleApplication.java
@@ -2,8 +2,10 @@ package com.wishfulcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer //启动一个服务注册中心提供给其他应用进行对话
 public class WishfulCloudEurekaSingleApplication {
 
 	public static void main(String[] args) {
